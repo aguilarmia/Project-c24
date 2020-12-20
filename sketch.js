c30dff81767eab332300319db1a3833a98f9fdf7
aguilarmia <73753457+aguilarmia@users.noreply.github.com>
2020-12-20 10:35:51 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,9 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var paper, ground, paper object 
+var engine,world;
+var paper, ground;
+var paperObject;
 function preload()
 {
 
@@ -11,22 +13,23 @@ function preload()
 
 function setup() {
 	createCanvas(800, 700);
-
-
 	engine = Engine.create();
 	world = engine.world;
 
-	//Create the Bodies Here.
+  paper = new Paper(70,25,50,50);
+  ground = new Ground(600,height,1200,20);
+  paperObject = new paperObject(70,70,50,50);
 
-
-	Engine.run(engine);
-  
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
+  
+  paper.display();
+  paperObject.display();
+  ground.display();
   
   drawSprites();
  
